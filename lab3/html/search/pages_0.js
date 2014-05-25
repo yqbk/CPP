83b0cd20038a7ff6e35e8b8af0bde42133c6ad7e
jaqbek <jksyrek@gmail.com>
2014-05-26 01:58:24 +0200

    Ready.

---
+++ lab3/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['Ćwiczenie_203_3a_20dziedziczenie_20i_20polimorfizm',['Ćwiczenie 3: Dziedziczenie i polimorfizm',['../index.html',1,'']]]
+];
--- lab3/html/search/search.js
+++ lab3/html/search/search.js
@@ -7,11 +7,12 @@
 
 var indexSectionsWithContent =
 {
-  0: "acefgilmnoprst~",
+  0: "acefgilmnoprst~Ä",
   1: "agt",
   2: "afgt",
   3: "acefglmnoprst~",
-  4: "is"
+  4: "is",
+  5: "Ä"
 };
 
 var indexSectionNames =
@@ -20,7 +21,8 @@ var indexSectionNames =
   1: "classes",
   2: "files",
   3: "functions",
-  4: "variables"
+  4: "variables",
+  5: "pages"
 };
 
 function convertToId(search)
