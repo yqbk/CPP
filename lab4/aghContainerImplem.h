35e1f78dab999c27319e0a83276d15e80e3e30d6
jaqbek <jksyrek@gmail.com>
2014-06-01 22:40:14 +0200

    do poprawki

---
+++ lab4/aghContainerImplem.h
@@ -0,0 +1,204 @@
+#ifndef AGHCONTAINERIMPLEM_H
+#define AGHCONTAINERIMPLEM_H
+
+
+//------------------------------------------------------------------------
+template<class T>
+aghContainer<T>::~aghContainer()
+{
+
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+void aghContainer<T>::append(T const& element)
+{
+	if(! insert( size(), element) ) 
+      throw aghException(0, "Operacja nie powiodla sie", __FILE__, __LINE__);    
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+bool aghContainer<T>::replace(int index, T const& element)
+{
+
+	bool wynik = false;
+
+	if(index < 0 || index >= size()) 
+    	throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+
+    else
+    {
+    	wynik = remove(index);
+
+    	if(wynik)
+        	insert(index, element);        	// <--
+    }
+
+    return wynik;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+void aghContainer<T>::clear(void)
+{
+	if( isEmpty() ) 
+      throw aghException(0, "Operacja nie powiodla sie", __FILE__, __LINE__);
+
+  	else
+    	while (remove(size()));
+}
+
+//------------------------------------------------------------------------
+template<class T>
+bool aghContainer<T>::isEmpty(void) const
+{
+    return ( size() <= 0 );
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+int aghContainer<T>::indexOf(T const& _value, int _from) const
+{
+	int wynik = -1;    
+
+	if(_from >= size() || _from < 0)	// ?
+      throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+
+  	else
+  	{
+    	for (int i = _from; i < size(); i++)    	
+        	if (at(i) == _value)        	
+            	return i;   	
+	}
+    return wynik;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+bool aghContainer<T>::contains(T const& _value, int _from) const
+{
+	bool wynik = false;
+
+	if(_from >= size() || _from < 0)	// ?
+      throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+
+  	else
+  	{
+    	if (indexOf(_value, _from) != -1)
+			return true;					// ?????
+    }
+    return wynik;
+}
+
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+template<class T>
+aghContainer<T> & aghContainer<T>::operator=(aghContainer<T> const & right)
+{
+	if(this != &right)
+	{
+		if(!this->isEmpty())
+			this->clear();
+
+		*this += right;
+	}
+
+	return *this;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+bool aghContainer<T>::operator==(aghContainer<T> const& right) const
+{
+	bool wynik = false;
+
+	wynik = (right.size() == size());
+
+	if (wynik)
+	{
+		for (int i = 0; wynik && i < size(); ++i)		
+			wynik = (right.at(i) == at(i));
+	}
+		
+	return wynik;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+bool aghContainer<T>::operator!=(aghContainer<T> const& right) const
+{
+	return !(*this == right);
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+aghContainer<T> & aghContainer<T>::operator+=(aghContainer<T> const& right)
+{
+   for(int i = 0; i < right.size(); i++) 
+      append(right[i]);
+   
+   return *this;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+aghContainer<T> & aghContainer<T>::operator+=(T const& element)
+{
+   append(element);
+   return *this;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+aghContainer<T> & aghContainer<T>::operator<<(T const& element)
+{
+	return *this += element;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+aghContainer<T> & aghContainer<T>::operator<<(aghContainer<T> const& right)
+{
+	return *this += right;
+}
+
+
+//------------------------------------------------------------------------
+/*
+template<class U> ostream& operator<<(ostream& stream, aghContainer<U> const& right) {
+   for(int i = 0; i < right.size(); i++) {
+      stream << right[i];
+      stream << endl;
+   }
+   return stream;
+}
+
+*/
+//------------------------------------------------------------------------
+
+template<class T>
+T & aghContainer<T>::operator[](int n) const
+{
+	if(n < 0 || n >= size())
+      throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+
+  	else
+    	return at(n);
+}
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+#endif
---
+++ lab4/dasd/aghVectorImplem.h
@@ -0,0 +1,210 @@
+#ifndef AGHVECTORIMPLEM_H
+#define AGHVECTORIMPLEM_H
+
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+template<class T>
+aghVector<T>::aghVector()
+{
+    dane = NULL;
+    iRozmiar = 1;
+    count = 0;    
+
+    resize(iRozmiar);
+
+}
+
+
+template<class T>
+aghVector<T>::aghVector(aghContainer<T> const & other) 
+{
+
+    dane = NULL;
+    iRozmiar = 1;
+    count = 0;   
+    
+    resize(iRozmiar);
+
+    this->aghContainer<T>::operator =(other);
+}
+
+template<class T>
+aghVector<T>::~aghVector()
+{
+    kasuj();
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+void aghVector<T>::powieksz()
+{
+    resize(iRozmiar + 1);
+    iRozmiar++;
+}
+
+template<class T>
+int aghVector<T>::wolneMiejsce()
+{
+    return iRozmiar - count;
+}
+
+
+//------------------------------------------------------------------------
+
+template<class T>
+void aghVector<T>::resize(int nowyRozmiar)
+{
+    if(nowyRozmiar < 0)
+      throw aghException(3, "Blad pamieci", __FILE__, __LINE__);    
+
+    else
+    {
+        T *temp = new T[nowyRozmiar];
+
+        int ileEl = size() < nowyRozmiar ? size() : nowyRozmiar;
+
+        if(dane != NULL) 
+        {
+            int roznica = nowyRozmiar - size();
+            
+            if (roznica < 0)      
+                for (int i = size(); i > nowyRozmiar ; --i)                
+                    dane[i].~T(); 
+
+            memcpy(temp, dane, nowyRozmiar*sizeof(T) ); 
+            
+            this->kasuj();
+        }                       
+    
+        this->iRozmiar = nowyRozmiar;
+        this->count = ileEl;        
+        this->dane = temp;
+
+
+    }
+}
+
+
+
+template<class T>
+void aghVector<T>::kasuj()
+{
+    if( dane != NULL)
+        delete [] dane;
+
+    dane = NULL;
+}
+
+
+//------------------------------------------------------------------------
+template<class T>
+int aghVector<T>::size() const
+{
+   return count;
+}
+
+template<class T>
+T& aghVector<T>::at(int index) const
+{
+    if( (index < 0) || (index >= iRozmiar) ) 
+        throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+
+    else
+        return dane[index];   
+}
+
+
+template<class T>
+bool aghVector<T>::insert(int index, T const& element)
+{
+    if( (index < 0)) 
+        throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+
+    else
+    {
+        if(index > iRozmiar)
+            for (int i = 0; i < index - iRozmiar; ++i)
+                powieksz();
+
+        if( wolneMiejsce() == 0)            
+            powieksz();
+
+
+        for (int i = index + 1; i <= size(); ++i)         
+            dane[i] = dane[i-1];    
+        
+        dane[index] = element;
+        count++;
+        
+        return true;
+    }
+
+   return false;
+}
+
+
+template<class T>
+bool aghVector<T>::remove(int index) 
+{   
+    if( (index < 0)) 
+        //throw aghException(1, "Niepoprawny index", __FILE__, __LINE__);
+        cout << "\ntralala\n";
+
+    else
+    {
+        if(index >= 0 && index <= iRozmiar)
+        {
+            if(size() > 0)
+            {
+                T *temp= new T[iRozmiar -1];
+
+                int j=0;
+                for(int i=0; i<size(); i++)
+                {
+                    if (i != index)
+                    {
+                        temp[j]=dane[i];
+                        j++;
+                    }
+                }
+
+                delete [] dane;
+                dane = temp;
+                count--;
+                iRozmiar--;
+/*
+
+                for (int i = index; i < size(); ++i)
+                    dane[i] = dane[i+1];
+
+                //dane[size()].~T(); 
+
+                resize(iRozmiar - 1);
+                count--;
+                */
+            }
+
+            return true; 
+        
+        }
+        else
+            cout << "\n\nwyskakuje\n";
+
+    }
+
+   return false;
+}
+
+template<class T>
+aghVector<T>& aghVector<T>:: operator=(aghVector<T> const& right){
+
+this->aghContainer<T>::operator =(right);
+   return *this;
+
+}
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+#endif
